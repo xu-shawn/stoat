cf3dcda4226c8d2f525b6b964d8280c5e0b8fcc8
Shawn Xu <xu107288696@gmail.com>
2025-02-05 19:53:18 -0800

    psqt

---
+++ src/eval/psqt.h
@@ -0,0 +1,143 @@
+/*
+ * Stoat, a USI shogi engine
+ * Copyright (C) 2025 Ciekce
+ *
+ * Stoat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Stoat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Stoat. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "../types.h"
+
+#include "../core.h"
+
+namespace stoat::eval {
+    namespace values {
+        using PSQT = std::array<Score, 81>;
+        // clang-format off
+        constexpr PSQT kPawn = {
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+             90,  90,  90,  90,  90,  90,  90,  90,  90,
+             80,  80,  81,  80,  80,  80,  80,  81,  80,
+             40,  40,  50,  40,  40,  40,  40,  50,  10,
+             20,  20,  30,  20,  20,  20,  20,  30,  20,
+             10,  10,  20,  15,  10,  10,  10,  20,  10,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+        };
+        constexpr PSQT kPromotedPawn = {
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+            -20, -20, -20, -20, -20, -20, -20, -20, -20,
+            -20, -20, -20, -20, -20, -20, -20, -20, -20,
+            -20, -20, -20, -20, -20, -20, -20, -20, -20,
+            -20, -20, -20, -20, -20, -20, -20, -20, -20,
+            -20, -20, -20, -20, -20, -20, -20, -20, -20,
+        };
+        constexpr PSQT kPromotedLance = kPromotedPawn;
+        constexpr PSQT kPromotedKnight = kPromotedPawn;
+        constexpr PSQT kPromotedSilver = kPromotedPawn;
+        constexpr PSQT kLance = {
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+             90,  90,  90,  90,  90,  90,  90,  90,  90,
+             80,  80,  80,  80,  80,  80,  80,  80,  80,
+             40,  40,  40,  40,  40,  40,  40,  40,  40,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+        };
+        constexpr PSQT kKnight = {
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+             90,  90,  90,  90,  90,  90,  90,  90,  90,
+             80,  80,  80,  80,  80,  80,  80,  80,  80,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+             10,  10,  10,  10,  10,  10,  10,  10,  10,
+        };
+        constexpr PSQT kSilver = {
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+        };
+        constexpr PSQT kGold = {
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+        };
+        constexpr PSQT kBishop = {
+            100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100,
+             70,  70,  70,  70,  70,  70,  70,  70,  70,
+             70,  70,  70,  50,  50,  50,  70,  70,  70,
+             70,  70,  50,  50,  30,  50,  50,  70,  70,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,  30,   0,   0,   0,   0,   0,   0,   0,
+            -10, -10, -10, -10, -10, -10, -10, -10, -10,
+        };
+        constexpr PSQT kPromotedBishop = kBishop;
+        constexpr PSQT kRook = {
+            100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100,
+             70,  70,  70,  70,  70,  70,  70,  70,  70,
+             50,  50,  50,  50,  50,  50,  50,  50,  70,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+              0,   0,   0,   0,   0,   0,   0,   0,   0,
+            -10, -10, -10, -10, -10, -10, -10, -10, -10,
+        };
+        constexpr PSQT kPromotedRook = kRook;
+        // clang-format on
+    } // namespace values
+    [[nodiscard]] constexpr Score pieceValue(PieceType pt, Color stm, Square sq) {
+        constexpr std::array kValues = {
+            values::kPawn,
+            values::kPromotedPawn,
+            values::kLance,
+            values::kKnight,
+            values::kPromotedLance,
+            values::kPromotedKnight,
+            values::kSilver,
+            values::kPromotedSilver,
+            values::kGold,
+            values::kBishop,
+            values::kRook,
+            values::kPromotedBishop,
+            values::kPromotedRook,
+        };
+
+        assert(pt);
+        return kValues[pt.idx()][stm == Colors::kWhite ? sq.rotate() : sq.idx()];
+    }
+} // namespace stoat::eval
