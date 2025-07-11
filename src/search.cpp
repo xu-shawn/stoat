7eb2320187455127609c2f7b31ab98364772b48a
Shawn Xu <xu107288696@gmail.com>
2025-07-10 22:43:41 -0700

    complexity lmr

--- src/search.cpp
+++ src/search.cpp
@@ -718,6 +718,7 @@ namespace stoat {
                 r -= move.isDrop() && Square::chebyshev(move.to(), pos.kingSq(pos.stm().flip())) < 3;
                 r += !improving;
                 r -= history / 8192;
+                r -= std::min(complexity / 15, 2);
 
                 const auto reduced = std::min(std::max(newDepth - r, 1), newDepth - 1);
                 score = -search(thread, newPos, curr.pv, reduced, ply + 1, -alpha - 1, -alpha, true);
