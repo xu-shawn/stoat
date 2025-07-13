2f00546940180b8be6d0d5524c771967b176eee8
Shawn Xu <xu107288696@gmail.com>
2025-07-13 04:04:43 -0700

    lmr pv node

--- src/search.cpp
+++ src/search.cpp
@@ -734,7 +734,7 @@ namespace stoat {
                 r += !improving;
                 r -= history / 8192;
 
-                const auto reduced = std::min(std::max(newDepth - r, 1), newDepth - 1);
+                const auto reduced = std::min(std::max(newDepth - r, 1), newDepth - 1) + kPvNode;
                 curr.reduction = newDepth - reduced;
                 score = -search(thread, newPos, curr.pv, reduced, ply + 1, -alpha - 1, -alpha, true);
                 curr.reduction = 0;
